53c0a289614b819706fe0600eb13e33009a77972
Sergei Frolov <serfro@tlu.ee>
2018-02-08 10:33:35 +0200

    proov2

--- Klassitöö0208/src/klassitöö/Proov.java
+++ Klassitöö0208/src/klassitöö/Proov.java
@@ -9,8 +9,11 @@ public class Proov {
     public static void main(String[] args) {
         Pall p1=new Pall(3,5,2);
         Pall p2=new Pall(4,6,3);
-        
-        System.out.println(p1);
+        Pall p3=new Pall(-10,8,2);
+        if(p1.kasPuutub(p2)){System.out.println("1 ja 2 puutuvad");}
+        if(p1.kasPuutub(p3)){System.out.println("1 ja 3 puutuvad");}
+        else {System.out.println("1 ja 3 ei puutu");}
+        System.out.println(p1 + "Kaugus algkohast: "+p1.kaugusNullist());
         System.out.println(p2);
         
     }
