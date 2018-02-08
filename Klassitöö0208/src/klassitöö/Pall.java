9ae37d86589fef3331bf55b5ef3c789877d092dd
Sergei Frolov <serfro@tlu.ee>
2018-02-08 10:38:13 +0200

    proov2

--- Klassitöö0208/src/klassitöö/Pall.java
+++ Klassitöö0208/src/klassitöö/Pall.java
@@ -11,19 +11,37 @@ package klassitöö;
  */
 public class Pall {
 
-    protected double x;
-    protected double y;
-    protected double r;
-    
-    public Pall (double x, double y, double r){
-        this.x=x;
-        this.y=y;
-        this.r=r;
-        
-       
-        
-        
-    }
-    
-    
+   protected double x;    
+   protected double y;    
+   protected double r;
+   //Lisage pallile raadius
+   //Lisage käsk kontrollimaks, kas kaks palli puutuvad kokku
+   
+   public Pall(double x, double y, double r){
+       this.x=x;
+       this.y=y;
+       this.r=r;
+   }
+   
+   public Pall(double x, double y){
+       this(x, y, 1);
+   }
+   public boolean kasPuutub(Pall teine){
+       return kaugusPallist(teine)<this.getR()+teine.getR();
+   }
+   public double kaugusNullist(){
+      return Math.sqrt(x*x+y*y);
+   }
+   public double getX(){return x;}
+   public double getY(){return y;}
+   public double getR(){return r;}
+   public double kaugusPallist(Pall teine){
+       double dx=getX()-teine.getX();
+       double dy=getY()-teine.getY();
+       return Math.sqrt(dx*dx+dy*dy);
+   }
+   @Override
+   public String toString(){
+      return "Pall kohal ("+x+", "+y+")";
+   }
 }
