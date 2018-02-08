c2b196c84fa6c116f1197fe369ed89e05c37facb
Sergei Frolov <serfro@tlu.ee>
2018-02-08 09:48:15 +0200

    pall

---
+++ Klassitöö0208/src/klassitöö/Pall.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package klassitöö;
+
+/**
+ *
+ * @author student
+ */
+public class Pall {
+
+    protected double x;
+    protected double y;
+    protected double r;
+    
+    public Pall (double x, double y, double r){
+        this.x=x;
+        this.y=y;
+        this.r=r;
+        
+       
+        
+        
+    }
+    
+    
+}
---
+++ Klassitöö0208/src/klassitöö/Proov.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package klassitöö;
+
+public class Proov {
+    public static void main(String[] args) {
+        Pall p1=new Pall(3,5,2);
+        Pall p2=new Pall(4,6,3);
+        
+        System.out.println(p1);
+        System.out.println(p2);
+        
+    }
+}
